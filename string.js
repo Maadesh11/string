62cfe92575727b37296ceb7e1cd0d23d0343dfd4
maadesh <151980884+Maadesh11@users.noreply.github.com>
2024-06-13 11:54:45 +0530

    Create string.js

---
+++ string.js
@@ -0,0 +1,15 @@
+let str='Maadesh';
+let str1='kumar'
+document.write(str+str1,"<br>");
+let s=str.charAt(6);
+document.write(s,"<br>");
+document.write(str.toUpperCase(),"<br>");
+document.write(str.toLowerCase(),"<br>");
+document.write(str.length,"<br>");
+document.write(str1.length,"<br>")
+document.write(str.indexOf('a'),"<br>")
+document.write(str1.lastIndexOf('a'),"<br>")
+document.write(str.substring(1,3));
+document.write(str==str1);
+document.write(str.replace('Maadesh','venkata'));
+document.write(str.concat('paritala',str,'kumar'));
